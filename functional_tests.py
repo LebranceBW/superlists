a43ffd5ee2dac56bad48672b47fd8820052fa1b7
LebranceBW <qinghuabw@163.com>
2016-08-19 19:12:55 +0800

    r

---
+++ functional_tests.py
@@ -0,0 +1,16 @@
+from selenium import webdriver;
+import unittest;
+from distutils.dist import warnings
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox();
+        self.browser.implicitly_wait(3);
+    def tearDown(self):
+        self.browser.quit();
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000");
+        self.assertIn('To-DO', self.browser.title);
+        self.fail('Finish the test!');
+        
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
