7e7126a7dc03a54b34248a1cafbc0b42a1cfd535
LebranceBW <qinghuabw@163.com>
2016-08-20 21:12:29 +0800

    返回todolist

--- lists/views.py
+++ lists/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 # from django.core.context_processors import request
 def home_page(request):
-    return render(request,'home.html')
+    return render(request,'home.html',{
+            'new_item_text':request.POST.get('item_text',''),
+    })
 # Create your views here.
