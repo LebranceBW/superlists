c9ae0ab5fbbc8e8615be87ca42fd280177ad2040
LebranceBW <qinghuabw@163.com>
2016-08-20 21:37:39 +0800

    仅能处理一个待办事项的网站

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,6 @@
 from selenium import webdriver;
 from selenium.webdriver.common.keys import Keys
-import time;
+#import time;
 import unittest;
 # from distutils.dist import warnings
 class NewVisitorTest(unittest.TestCase):
@@ -21,10 +21,15 @@ class NewVisitorTest(unittest.TestCase):
         )
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 #         time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        self.assertIn('2:Use peacock feathers to make a fly',[row.text for row in rows])
         
         self.fail('Finish the test!');
         
--- lists/views.py
+++ lists/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # from django.core.context_processors import request
 def home_page(request):
     return render(request,'home.html',{
-            'new_item_text':request.POST.get('item_text',''),
+            'new_item_text': request.POST.get('item_text',''),
     })
 # Create your views here.
